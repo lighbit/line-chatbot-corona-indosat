f1573f9dfd2f4dec16ffe6b9ed5444b09fdf71fd
zulkarnaen <zulkarnaen@multipolar.com>
2020-04-06 14:55:18 +0700

    fixing

--- src/main/java/com/zulkarnaen/bot/controller/CoronaBotController.java
+++ src/main/java/com/zulkarnaen/bot/controller/CoronaBotController.java
@@ -83,10 +83,10 @@ public class CoronaBotController {
 	@RequestMapping(value = "/webhook", method = RequestMethod.POST)
 	public ResponseEntity<String> callback(String xLineSignature, @RequestBody String eventsPayload) {
 		try {
-			// validasi line signature. matikan validasi ini jika masih dalam pengembangan
-//			if (!lineSignatureValidator.validateSignature(eventsPayload.getBytes(), xLineSignature)) {
-//				throw new RuntimeException("Invalid Signature Validation");
-//			}
+//			 validasi line signature. matikan validasi ini jika masih dalam pengembangan
+			if (!lineSignatureValidator.validateSignature(eventsPayload.getBytes(), xLineSignature)) {
+				throw new RuntimeException("Invalid Signature Validation");
+			}
 
 			System.out.println(eventsPayload);
 			ObjectMapper objectMapper = ModelObjectMapper.createNewObjectMapper();
